f606bc88adfbf63cefff2de0b93dc1da96be5af8
tyatyamaruko <ryotatanks9981@gmail.com>
2025-04-30 15:39:02 +0900

    del: 余計な記述を削除

--- config/nativephp.php
+++ config/nativephp.php
@@ -138,11 +138,10 @@ return [
      * Define your own scripts to run before and after the build process.
      */
     'prebuild' => [
-        // 'npm run build',
+        'npm run build', // Run a command before the build
+        'php artisan optimize', // Run another command before the build
     ],
-
     'postbuild' => [
-        // 'rm -rf public/build',
     ],
 
     /**
