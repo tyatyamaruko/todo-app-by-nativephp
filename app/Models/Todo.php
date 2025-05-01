603fc184c9ac3637bc18c53d219c1b9267c638f3
tyatyamaruko <ryotatanks9981@gmail.com>
2025-05-01 16:15:43 +0900

    fix: php側のビジネスロジックを削除

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -6,47 +6,4 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     protected $fillable = ['status', 'title', 'description'];
-
-    public function isPending(): bool
-    {
-        return $this->status === 'pending';
-    }
-
-    public function isProgress(): bool
-    {
-        return $this->status === 'in-progress';
-    }
-
-    public function isCompleted(): bool
-    {
-        return $this->status === 'done';
-    }
-
-    public function isOverdue(): bool
-    {
-        $now = now();
-        $deadline = $this->deadline;
-
-        if ($deadline) {
-            return $now->greaterThan($deadline);
-        }
-
-        return false;
-    }
-
-    public function isNothingDeadline(): bool
-    {
-        return $this->deadline === null;
-    }
-
-    public function dueToDays(): int
-    {
-        $now = now();
-        $deadline = $this->deadline;
-        if ($deadline) {
-            return (int) $now->diffInDays($deadline);
-        }
-
-        return 0;
-    }
 }
