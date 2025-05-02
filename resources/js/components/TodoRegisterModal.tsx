78c351a637a1bf5cdec5976c402a51b9dd747218
tyatyamaruko <ryotatanks9981@gmail.com>
2025-05-02 12:30:49 +0900

    fix: preview時のプレビューボタンを編集ボタンに切り替えるようにした　

--- resources/js/components/TodoRegisterModal.tsx
+++ resources/js/components/TodoRegisterModal.tsx
@@ -74,9 +74,11 @@ export default function TodoRegisterModal({ onClick }: Props) {
                 }
                 </div>
                 <div className="flex justify-end">
-                    <button onClick={() => setIsPreviewVisible((prev) => !prev)} className="bg-gray-300 text-gray-700 px-3 py-2 text-sm rounded-md mb-4 hover:bg-gray-400">
+                    {!isPreviewVisible ? <button onClick={() => setIsPreviewVisible((prev) => !prev)} className="bg-gray-300 text-gray-700 px-3 py-2 text-sm rounded-md mb-4 hover:bg-gray-400">
                         プレビュー
-                    </button>
+                    </button> : <button onClick={() => setIsPreviewVisible((prev) => !prev)} className="bg-gray-300 text-gray-700 px-3 py-2 text-sm rounded-md mb-4 hover:bg-gray-400">
+                        編集
+                    </button>}
                 </div>
                 <Calendar onChange={updateTaskDueDate}/>
                 <button onClick={addTask} className="bg-blue-500 text-white px-4 py-2 rounded-md w-full mt-2">追加</button>
